ede27b429031a32d6b5bcae068bcbd7d9fc539d8
Stan-breaks <stanleymwendwa03@gmail.com>
2025-04-02 16:49:37 +0300

    models
    index

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+// This file ensures all models are properly registered
+import { User } from './User';
+import { Artwork } from './Artwork';
+import { Bid } from './Bid';
+
+export { User, Artwork, Bid }; 
